9d194827689c2ae9e3951491da96013ea1e441b2
ErrorInever <errorinever@gmail.com>
2021-04-12 19:51:46 +0000

    add inference

--- inference.py
+++ inference.py
@@ -1,4 +1,6 @@
 import argparse
+import torchvision
+from matplotlib import pyplot as plt
 from model import Generator
 from utils import load_gen
 from config import cfg
@@ -6,7 +8,6 @@ from config import cfg
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='WGAN-GP')
-	parser.add_argument('--digit', dest='digit', help='the digit to be generated', default=None, type=str)
 	parser.add_argument('--num_samples', dest='num_samples', help='digits count', default=None, type=str)
 	parser.add_argument('--model_path', dest='model_path', help='path to weights of model', default=None, type=str)
 	return parser.parse_args()
@@ -16,12 +17,8 @@ if __name__ == '__main__':
 	args = parse_args
 	assert args.model_path, 'model_path not specified'
 
-	if args.digit is None:
-		digit = 6
-	else:
-		digit = args.digit
 	if args.num_samples is None:
-		num_samples = 16
+		num_samples = 32
 	else:
 		num_samples = args.num_samples
 
@@ -32,7 +29,13 @@ if __name__ == '__main__':
 	cfg.NUM_CLASSES, cfg.IMG_SIZE, cfg.GEN_EMBEDDING).to(device)
 	load_gen(gen, args.model_path)
 
-	noise = torch.randn(num_samples, cfg.LATENT_Z_DIMENSION, 1, 1, device=device)
-	labels = torch.empty(num_samples, 8).fill_(digit).to(device)
+	noise = torch.randn(num_samples, 100, 1, 1).to(device)
+	labels = torch.LongTensor(np.random.randint(0, 10, size=num_samples)).to(device)
 
-	data = gen(noise, labels)
+	fake_data = gen(noise, labels)
+
+	images = out
+	horizontal_grid = torchvision.utils.make_grid(images, normalize=True, scale_each=True)
+	nrows = int(np.sqrt(num_samples))
+	grid = torchvision.utils.make_grid(images, nrow=nrows, normalize=True, scale_each=True)
+	fig.savefig(f'result_{num_samples}')
